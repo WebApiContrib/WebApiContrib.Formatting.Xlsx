e70ae633ffa7032fb463ff12f1d69d423e38db18
jordangray <j.b.gray@gmail.com>
2014-01-08 23:16:32 +0000

    Initial commit. No functionality, just sketching out unit tests.

---
+++ ExcelWebApi/ExcelWebApi.Tests/ExcelMediaTypeFormatterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelWebApi.Tests
+{
+    [TestClass]
+    public class ExcelMediaTypeFormatterTests
+    {
+        [TestMethod]
+        public void SupportedMediaTypes_SupportsExcelMediaTypes()
+        {
+
+        }
+
+        [TestMethod]
+        public void CanWriteType_TypeCollection_CanWriteType()
+        {
+
+        }
+
+        [TestMethod]
+        public void CanWriteType_TypeObject_CannotWriteType()
+        {
+
+        }
+
+        [TestMethod]
+        public void WriteToStreamAsync_WithGenericCollection_WritesExcelDocumentToStream()
+        {
+
+        }
+
+        [TestMethod]
+        public void WriteToStreamAsync_WritesExcelDocumentToStream()
+        {
+
+        }
+    }
+}
