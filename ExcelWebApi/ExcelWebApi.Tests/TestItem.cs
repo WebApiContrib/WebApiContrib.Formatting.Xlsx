f43524b53a6604dd06033407840b63934880339b
Jordan Gray <jordan@jordangray.me>
2014-01-10 01:08:58 +0000

    TestItem class for unit testing.

---
+++ ExcelWebApi/ExcelWebApi.Tests/TestItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExcelWebApi.Tests
+{
+    [DataContract]
+    public class TestItem
+    {
+        [DataMember(Order = 1)]
+        [Excel]
+        public string Value1 { get; set; }
+
+        [DataMember(Order = 2)]
+        [Excel]
+        public string Value2 { get; set; }
+    }
+}
