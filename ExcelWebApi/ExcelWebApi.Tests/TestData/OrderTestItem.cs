a1faf42af4624ce86dff310ece59148c6ac72d44
Jordan Gray <jordan@jordangray.me>
2014-01-12 23:56:03 +0000

    Added OrderTestItem, will be used to test ExcelAttribute.Order.

---
+++ ExcelWebApi/ExcelWebApi.Tests/TestData/OrderTestItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using ExcelWebApi;
+
+namespace ExcelWebApi.Tests.TestData
+{
+    [DataContract]
+    public class OrderTestItem
+    {
+        [Excel]
+        public string Value1 { get; set; }
+
+        [Excel]
+        public string Value2 { get; set; }
+    }
+}
