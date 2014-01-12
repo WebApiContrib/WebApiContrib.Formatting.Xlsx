ad56c674005957a7bc8131453ec48401ac13ea91
Jordan Gray <jordan@jordangray.me>
2014-01-12 23:55:17 +0000

    Grouped test item classes in anticipation of additional tests.

---
+++ ExcelWebApi/ExcelWebApi.Tests/TestData/TestItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExcelWebApi.Tests.TestData
+{
+    [DataContract]
+    public class TestItem
+    {
+        [Excel(Order = 1)]
+        public string Value1 { get; set; }
+
+        [Excel(Order = 2)]
+        public string Value2 { get; set; }
+    }
+}
