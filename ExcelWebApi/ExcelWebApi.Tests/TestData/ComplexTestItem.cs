1fe9b2b6623e5fd2cfcb794a5cba4d426e42cfbc
Jordan Gray <j.b.gray@gmail.com>
2014-01-13 22:33:30 +0000

    Added new unit tests for Order and Ignore properties.

---
+++ ExcelWebApi/ExcelWebApi.Tests/TestData/ComplexTestItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+using ExcelWebApi;
+
+namespace ExcelWebApi.Tests.TestData
+{
+    public class ComplexTestItem
+    {
+        public string Value1 { get; set; }
+
+        [Excel(Order = 2)]
+        public string Value2 { get; set; }
+
+        [Excel(Order = 1)]
+        public string Value3 { get; set; }
+
+        [Excel(Order = -2)]
+        public string Value4 { get; set; }
+
+        public string Value5 { get; set; }
+
+        [Excel(Ignore = true)]
+        public string Value6 { get; set; }
+    }
+}
---
+++ ExcelWebApi/ExcelWebApi.Tests/TestData/SimpleTestItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExcelWebApi.Tests.TestData
+{
+    public class SimpleTestItem
+    {
+        public string Value1 { get; set; }
+
+        public string Value2 { get; set; }
+    }
+}
