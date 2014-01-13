94b62494cf003ae52ebda7ea2329df4f71825912
jordangray <j.b.gray@gmail.com>
2014-01-13 00:27:01 +0000

    Removed dependency on data member order!
    Conflicts:
    	ExcelWebApi/ExcelWebApi.Tests/FormatterUtilsTests.cs
    	ExcelWebApi/ExcelWebApi/ExcelAttribute.cs
    	ExcelWebApi/ExcelWebApi/ExcelFieldInfo.cs
    	ExcelWebApi/ExcelWebApi/FormatterUtils.cs

---
+++ ExcelWebApi/ExcelWebApi.Tests/FormatterUtilsTests.cs
@@ -0,0 +1,62 @@
+﻿using ExcelWebApi.Tests.TestData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ExcelWebApi.Tests
+{
+    [TestClass]
+    public class FormatterUtilsTests
+    {
+        [TestMethod]
+        public void GetAttribute_ExcelAttributeOfTestItem_ReturnsDataMemberAttribute()
+        {
+            var value1 = typeof(TestItem).GetMember("Value1")[0];
+            var excelAttribute = FormatterUtils.GetAttribute<ExcelAttribute>(value1);
+
+            Assert.IsNotNull(excelAttribute);
+            Assert.AreEqual(1, excelAttribute.Order);
+        }
+
+        [TestMethod]
+        public void MemberOrder_TestItem_ReturnsMemberOrder()
+        {
+            var testItemType = typeof(TestItem);
+            var value1 = testItemType.GetMember("Value1")[0];
+            var value2 = testItemType.GetMember("Value2")[0];
+
+            Assert.AreEqual(1, FormatterUtils.MemberOrder(value1), "Value1 should have order 1.");
+            Assert.AreEqual(2, FormatterUtils.MemberOrder(value2), "Value2 should have order 2.");
+        }
+
+        [TestMethod]
+        public void GetMemberNames_TestItem_ReturnsMemberNamesInOrder()
+        {
+            var memberNames = FormatterUtils.GetMemberNames(typeof(TestItem));
+
+            Assert.IsNotNull(memberNames);
+            Assert.AreEqual(2, memberNames.Count);
+            Assert.AreEqual("Value1", memberNames[0]);
+            Assert.AreEqual("Value2", memberNames[1]);
+        }
+
+        [TestMethod]
+        public void GetMemberInfo_TestItem_ReturnsMemberInfoList()
+        {
+            var memberInfo = FormatterUtils.GetMemberInfo(typeof(TestItem));
+
+            Assert.IsNotNull(memberInfo);
+            Assert.AreEqual(2, memberInfo.Count);
+        }
+
+        [TestMethod]
+        public void GetEnumerableItemType_ListOfTestItem_ReturnsTestItemType()
+        {
+            var testItemList = new List<TestItem>();
+            var itemType = FormatterUtils.GetEnumerableItemType(testItemList);
+
+            Assert.IsNotNull(itemType);
+            Assert.AreEqual(typeof(TestItem), itemType);
+        }
+    }
+}
---
+++ ExcelWebApi/ExcelWebApi/ExcelFieldInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace ExcelWebApi
+{
+    public class ExcelFieldInfo
+    {
+        public string PropertyName { get; set; }
+        public ExcelAttribute ExcelAttribute { get; set; }
+        public string FormatString { get; set; }
+        public string Header { get; set; }
+
+        public string ExcelNumberFormat
+        {
+            get { return ExcelAttribute != null ? ExcelAttribute.NumberFormat : null; }
+        }
+
+        public bool IsExcelHeaderDefined
+        {
+            get { return ExcelAttribute != null && !string.IsNullOrEmpty(ExcelAttribute.Header); }
+        }
+
+        public ExcelFieldInfo(string propertyName, ExcelAttribute excelAttribute = null, string formatString = null)
+        {
+            PropertyName = propertyName;
+            ExcelAttribute = excelAttribute;
+            FormatString = formatString;
+            Header = IsExcelHeaderDefined ? ExcelAttribute.Header : propertyName;
+        }
+    }
+}
