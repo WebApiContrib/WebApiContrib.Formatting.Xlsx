52232d564717d7485ccbbd3793a48f7a62cbf9bd
Jordan Gray <j.b.gray@gmail.com>
2014-01-28 00:01:29 +0000

    Make ComplexTestItem class a little more complex

--- XlsxForWebApi/XlsxForWebApi.Tests/TestData/ComplexTestItem.cs
+++ XlsxForWebApi/XlsxForWebApi.Tests/TestData/ComplexTestItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 using XlsxForWebApi;
 
 namespace XlsxForWebApi.Tests.TestData
@@ -6,18 +7,24 @@ namespace XlsxForWebApi.Tests.TestData
     [ExcelDocument("Complex test item")]
     public class ComplexTestItem
     {
+        public enum TestEnum
+	    {
+	        First,
+            Second
+	    }
+
         public string Value1 { get; set; }
 
         [ExcelColumn(Order = 2)]
-        public string Value2 { get; set; }
+        public DateTime Value2 { get; set; }
 
-        [ExcelColumn(Order = 1)]
-        public string Value3 { get; set; }
+        [ExcelColumn(Header = "Header 3", Order = 1)]
+        public bool Value3 { get; set; }
 
-        [ExcelColumn(Order = -2)]
-        public string Value4 { get; set; }
+        [ExcelColumn(Order = -2, NumberFormat="???.???")]
+        public double Value4 { get; set; }
 
-        public string Value5 { get; set; }
+        public TestEnum Value5 { get; set; }
 
         [ExcelColumn(Ignore = true)]
         public string Value6 { get; set; }
